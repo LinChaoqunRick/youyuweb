27d613d3c2bf118836d1d802ebd8b4ca6b012ab6
LinChaoqunRick <602314134@qq.com>
2022-09-13 21:44:20 +0800

    ''

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
+import Antd from 'ant-design-vue';
 
-import './assets/main.css'
+import './assets/main.css';
+import 'ant-design-vue/dist/antd.css';
 
 const app = createApp(App)
 
 app.use(router)
+app.use(Antd)
 
 app.mount('#app')
