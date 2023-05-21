7fb3e173f23d61845fb14b1e0bee5f380308e42e
LinChaoqunRick <602314134@qq.com>
2023-05-21 13:27:25 +0800

    feat:moment

--- src/views/moment/components/MomentEditor.vue
+++ src/views/moment/components/MomentEditor.vue
@@ -1,16 +1,40 @@
 <template>
   <div class="moment-editor" :class="{'editor-active': active}">
     <div class="editor-body">
+      <div class="login-mask" @click="toLogin" v-if="!isLogin"></div>
       <div class="editor-content">
-        <div class="rich-editor" contenteditable @focus="onFocus" @blur="onBlur" :maxLength="10"></div>
+        <div class="rich-editor"
+             contenteditable
+             @focus="onFocus"
+             @blur="onBlur"
+             @input="onInput"
+             placeholder="此刻在想什么..."
+             :maxLength="10"></div>
       </div>
       <div class="topic-wrapper">
         <div class="now-mood">
           <i-ulikecam theme="multi-color" size="13" :fill="['#1890ff' ,'#fff' ,'#1890ff' ,'#1890ff']"/>
           <span class="mood-text">现在的心情</span>
-          <i-right theme="outline" size="13" :fill="currentColor"/>
+          <i-right theme="outline" size="13" fill="currentColor"/>
+        </div>
+        <div class="word-counter">{{currentLength}}/{{maxLength}}</div>
+      </div>
+    </div>
+    <div class="image-wrapper" v-if="moment.images?.length">
+      <div v-for="item in moment.images" class="image-item">
+        <img :src="item"/>
+        <div class="image-delete" @click="onImageDelete(index)">
+          <i-close theme="outline" size="10" fill="currentColor" :strokeWidth="2"/>
         </div>
       </div>
+      <UploadFile accept=".jpg, .jpeg, .png"
+                  @uploadSuccess="uploadSuccess"
+                  :disabled="uploadDisabled"
+                  v-if="!uploadDisabled">
+        <div class="upload-image" @click="onUpload">
+          <i-plus theme="outline" size="40" fill="currentColor" :strokeWidth="1"/>
+        </div>
+      </UploadFile>
     </div>
     <div class="editor-bottom">
       <div class="tool-items">
@@ -18,10 +42,12 @@
           <i-emotion-happy theme="outline" size="16" fill="currentColor" :strokeWidth="3"/>
           <span class="tool-title">表情</span>
         </div>
-        <div class="tool-item">
-          <i-add-picture theme="outline" size="16" fill="currentColor" :strokeWidth="3"/>
-          <span class="tool-title">图片</span>
-        </div>
+        <UploadFile accept=".jpg, .jpeg, .png" @uploadSuccess="uploadSuccess" :disabled="uploadDisabled">
+          <div class="tool-item item-upload-image">
+            <i-add-picture theme="outline" size="16" fill="currentColor" :strokeWidth="3"/>
+            <span class="tool-title">图片</span>
+          </div>
+        </UploadFile>
         <div class="tool-item">
           <i-topic theme="outline" size="16" fill="currentColor" :strokeWidth="3"/>
           <span class="tool-title">话题</span>
@@ -32,30 +58,87 @@
 </template>
 
 <script setup lang="ts">
-  import {ref} from 'vue';
+  import {ref, reactive, computed} from 'vue';
+  import {useStore} from "vuex";
+  import UploadFile from '@/components/common/utils/upload/UploadFile.vue';
+
+  const {getters, commit} = useStore();
+  const userInfo = computed(() => getters['userInfo']);
+  const isLogin = computed(() => getters['isLogin']);
+  const maxLength = 100;
+  const currentLength = computed(() => moment.content.length);
+  const uploadDisabled = computed(() => moment.images.length >= 9)
 
   const active = ref(false);
+  const moment = reactive({
+    userId: userInfo.value.id,
+    content: '',
+    images: [
+      'https://youyu-source.oss-cn-beijing.aliyuncs.com/avatar/10000/20210203110431mario.jpg',
+      'https://youyu-source.oss-cn-beijing.aliyuncs.com/post/images/2023/0513/20230513105651_%E5%A3%81%E7%BA%B81.jpg',
+      'https://youyu-source.oss-cn-beijing.aliyuncs.com/post/images/2023/0513/20230513105651_%E5%A3%81%E7%BA%B81.jpg',
+      'https://youyu-source.oss-cn-beijing.aliyuncs.com/post/images/2023/0513/20230513105651_%E5%A3%81%E7%BA%B81.jpg',
+      'https://youyu-source.oss-cn-beijing.aliyuncs.com/post/images/2023/0513/20230513105651_%E5%A3%81%E7%BA%B81.jpg',
+      'https://youyu-source.oss-cn-beijing.aliyuncs.com/post/images/2023/0513/20230513105651_%E5%A3%81%E7%BA%B81.jpg',
+      'https://youyu-source.oss-cn-beijing.aliyuncs.com/post/images/2023/0513/20230513105651_%E5%A3%81%E7%BA%B81.jpg',
+      'https://youyu-source.oss-cn-beijing.aliyuncs.com/post/images/2023/0513/20230513105651_%E5%A3%81%E7%BA%B81.jpg',
+      'https://youyu-source.oss-cn-beijing.aliyuncs.com/post/images/2023/0513/20230513105651_%E5%A3%81%E7%BA%B81.jpg',
+    ]
+  });
 
   const onFocus = () => {
     active.value = true;
+    console.log(userInfo);
   }
   const onBlur = () => {
     active.value = false;
+  }
+
+  const onInput = ($event: Event) => {
+    const content = $event.target.innerText;
+    if (content.length >= maxLength) {
+      moment.content = moment.content.substring(0, 100);
+      return;
+    }
+    moment.content = $event.target.innerText;
+  }
+
+  const uploadSuccess = (fileList: []) => {
+    const url = fileList[0].url;
+    moment.images.push(url);
+  }
+
+  const toLogin = () => {
+    commit("changeLogin", true);
+  }
+
+  const onImageDelete = (index: number) => {
+    moment.images.splice(index, 1);
   }
 </script>
 
 <style lang="scss" scoped>
   .moment-editor {
-    width: 720px;
+    width: 750px;
     padding: 20px 20px 0;
     background-color: var(--youyu-background1);
     border-radius: 4px;
 
     .editor-body {
+      position: relative;
       background: var(--youyu-background2);
       border-radius: 4px;
       transition: .3s;
       border: 1px solid transparent;
+
+      .login-mask {
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        z-index: 1;
+      }
 
       .editor-content {
         position: relative;
@@ -73,6 +156,11 @@
           padding: 5px 12px;
           box-sizing: border-box;
           white-space: pre-wrap;
+
+          &[contenteditable]:empty:before {
+            content: attr(placeholder);
+            color: #8a919f;
+          }
         }
       }
 
@@ -80,6 +168,7 @@
         height: 32px;
         display: flex;
         align-items: center;
+        justify-content: space-between;
         font-size: 12px;
 
         .now-mood {
@@ -106,11 +195,73 @@
             padding-left: 4px;
           }
         }
+
+        .word-counter {
+          font-size: 13px;
+          color: var(--youyu-text1);
+          padding-right: 10px;
+        }
+      }
+    }
+
+    .image-wrapper {
+      display: flex;
+      flex-wrap: wrap;
+
+      .image-item {
+        position: relative;
+        margin: 8px 8px 0 0;
+
+        img {
+          height: 80px;
+          width: 80px;
+          object-fit: cover;
+        }
+
+        .image-delete {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          cursor: pointer;
+          width: 18px;
+          height: 18px;
+          position: absolute;
+          top: 4px;
+          right: 4px;
+          border-radius: 50%;
+          border: 1px solid #c5c5c5;
+          background: rgba(0, 0, 0, .4);
+
+          &:hover {
+            background: rgba(0, 0, 0, .3);
+          }
+
+          .i-icon {
+            color: white;
+          }
+        }
+      }
+
+      .upload-image {
+        cursor: inherit;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 80px;
+        width: 80px;
+        border: 1px dashed #c5c5c5;
+        margin: 8px 8px 8px 0;
+        background: rgba(248, 248, 249, 0.2);
+        transition: .3s;
+
+        &:hover {
+          background: rgba(248, 248, 249, 0.3);
+        }
       }
     }
 
     .editor-bottom {
-      padding: 16px 0;
+      padding: 10px 0;
 
       .tool-items {
         display: flex;
@@ -120,7 +271,7 @@
         .tool-item {
           display: flex;
           align-items: center;
-          cursor: pointer;
+          cursor: inherit;
           margin-right: 16px;
           transition: .3s;
 
@@ -138,6 +289,10 @@
             color: #1890ff;
           }
         }
+
+        ::v-deep(.avatar-uploader) {
+          display: flex;
+        }
       }
     }
   }
@@ -150,6 +305,14 @@
       .now-mood {
         background: #eaf2ff !important;
       }
+    }
+  }
+
+  ::v-deep(.ant-upload-disabled) {
+    cursor: not-allowed !important;
+
+    .item-upload-image {
+      color: #8a919f !important;
     }
   }
 </style>
