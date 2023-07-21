fe7049e7138e20333c87b093bcd4e6857661df24
youyu <74580424+LinChaoqunRick@users.noreply.github.com>
2023-07-21 11:43:30 +0800

    fix: prodution error:runtime-core.esm-bundler.js:244 TypeError: Cannot read properties of undefined (reading 'refs')

--- src/components/common/system/EmptyPage.vue
+++ src/components/common/system/EmptyPage.vue
@@ -4,40 +4,40 @@
       <component :is="Component"
                  v-if="$route.meta.keepAlive && isRouterAlive"
                  :key="$route.name"
-                 ref="Component"></component>
+                 ref="ComponentRef"></component>
     </keep-alive>
     <component :is="Component"
                v-if="!$route.meta.keepAlive && isRouterAlive"
                :key="$route.name"
-               ref="Component"></component>
+               ref="ComponentRef"></component>
   </router-view>
 </template>
 
 <script lang="ts">
-  export default {
-    name: "EmptyPage",
-  }
+export default {
+  name: "EmptyPage",
+}
 </script>
 
 <script setup lang="ts">
-  import {nextTick, provide, ref} from "vue";
+import {nextTick, provide, ref} from "vue";
 
-  const isRouterAlive = ref<boolean>(true);
+const isRouterAlive = ref<boolean>(true);
 
-  const Component = ref(null);
+const ComponentRef = ref(null);
 
-  function reload() {
-    isRouterAlive.value = false;
-    nextTick(function () {
-      isRouterAlive.value = true;
-    })
-  }
-
-  provide('reload', reload);
-
-  defineExpose({
-    Component
+function reload() {
+  isRouterAlive.value = false;
+  nextTick(function () {
+    isRouterAlive.value = true;
   })
+}
+
+provide('reload', reload);
+
+defineExpose({
+  ComponentRef
+})
 </script>
 
 <style scoped>
