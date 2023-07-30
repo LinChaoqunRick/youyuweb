437cddf7cb8242fc720d1021e1dcf62f79b502c2
youyu <74580424+LinChaoqunRick@users.noreply.github.com>
2023-07-30 16:24:35 +0800

    pref: 优化文字描述

--- src/components/common/system/ContentData.vue
+++ src/components/common/system/ContentData.vue
@@ -1,77 +1,80 @@
 <template>
-  <div class="content-data" :style="{minHeight: minHeight+'px'}">
+  <div class="content-data">
     <a-spin :spinning="loading">
       <slot :data="data">
-        <div v-if="failed" @click="initData" :style="{minHeight: minHeight+'px'}" class="retry-load">
-          <i-refresh theme="outline" size="15" fill="#1890ff"/>
-          <span class="loading-text">重新加载</span>
+        <div class="bottom-operation">
+          <div v-if="failed" @click="initData" class="retry-load">
+            <i-refresh theme="outline" size="15" fill="#1890ff"/>
+            <span class="loading-text">加载失败，重新加载</span>
+          </div>
+          <div v-else-if="!loading && (!data || !data?.length)" class="no-data">暂无数据</div>
         </div>
-        <div v-else-if="!loading && (!data || !data?.length)" class="no-data" :style="{minHeight: minHeight+'px'}">暂无数据</div>
       </slot>
     </a-spin>
   </div>
 </template>
 
 <script lang="ts" setup>
-  import {ref} from 'vue'
-  import {useStore} from "vuex";
+import {ref} from 'vue'
+import {useStore} from "vuex";
 
-  const props = defineProps({
-    url: {
-      type: String,
-      required: true
-    },
-    params: {
-      type: Object,
-    },
-    showSpin: {
-      type: Boolean,
-      default: true
-    },
-    minHeight: {
-      type: Number,
-      default: 200
-    }
+const props = defineProps({
+  url: {
+    type: String,
+    required: true
+  },
+  params: {
+    type: Object,
+  },
+  showSpin: {
+    type: Boolean,
+    default: true
+  },
+})
+
+const {dispatch} = useStore();
+
+const loading = ref<boolean>(false);
+const failed = ref<boolean>(false);
+
+const data = ref();
+
+const initData = () => {
+  loading.value = true;
+  failed.value = false;
+  dispatch(props.url, props.params).then(res => {
+    data.value = res.data;
+  }).catch(() => {
+    failed.value = true;
+  }).finally(() => {
+    loading.value = false;
   })
+}
 
-  const {dispatch} = useStore();
+initData();
 
-  const loading = ref<boolean>(false);
-  const failed = ref<boolean>(false);
-
-  const data = ref();
-
-  const initData = () => {
-    loading.value = true;
-    failed.value = false;
-    dispatch(props.url, props.params).then(res => {
-      data.value = res.data;
-    }).catch(() => {
-      failed.value = true;
-    }).finally(() => {
-      loading.value = false;
-    })
-  }
-
-  initData();
-
-  defineExpose({
-    data,
-    initData
-  })
+defineExpose({
+  data,
+  initData
+})
 </script>
 
 <style lang="scss" scoped>
-  .content-data {
-    position: relative;
+.content-data {
+  position: relative;
 
-    ::v-deep(.ant-spin) {
-      min-height: 200px;
-      width: 100%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
+  ::v-deep(.ant-spin) {
+    min-height: 200px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .bottom-operation {
+    height: 48px;
+    background-color: var(--youyu-background2);
+    margin: 8px 16px;
 
     .retry-load {
       width: 100%;
@@ -94,4 +97,5 @@
       width: 100%;
     }
   }
+}
 </style>
