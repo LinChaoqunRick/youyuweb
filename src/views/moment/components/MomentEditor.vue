007aa5acda3babc0b5bfb31aacc93e631005be32
LinChaoqunRick <602314134@qq.com>
2023-08-15 21:45:09 +0800

    fix: 修复上传图片后不回显的问题

--- src/views/moment/components/MomentEditor.vue
+++ src/views/moment/components/MomentEditor.vue
@@ -70,296 +70,298 @@
 </template>
 
 <script setup lang="ts">
-  import {ref, reactive, computed, toRaw} from 'vue';
-  import {useStore} from "vuex";
-  import {message} from 'ant-design-vue';
-  import {cloneDeep} from 'lodash';
-  import {vOnClickOutside} from '@vueuse/components'
-  import {onCheckLogin} from "@/assets/utils/utils";
-  import {transformHTMLToTag} from "@/components/common/utils/emoji/youyu_emoji";
-  import UploadFile from '@/components/common/utils/upload/UploadFile.vue';
-  import EmojiPicker from "@/components/common/utils/emoji/EmojiPicker.vue";
-  import ContentEditableDiv from "@/components/common/utils/contenteditable/ContentEditableDiv.vue";
+import {ref, reactive, computed, toRaw} from 'vue';
+import {useStore} from "vuex";
+import {message} from 'ant-design-vue';
+import {cloneDeep} from 'lodash';
+import {vOnClickOutside} from '@vueuse/components'
+import {onCheckLogin} from "@/assets/utils/utils";
+import {transformHTMLToTag} from "@/components/common/utils/emoji/youyu_emoji";
+import UploadFile from '@/components/common/utils/upload/UploadFile.vue';
+import EmojiPicker from "@/components/common/utils/emoji/EmojiPicker.vue";
+import ContentEditableDiv from "@/components/common/utils/contenteditable/ContentEditableDiv.vue";
 
-  const props = defineProps({
-    form: {
-      type: Object,
-      default: () => ({
+const props = defineProps({
+  form: {
+    type: Object,
+    default() {
+      return reactive({
         content: '',
         images: []
       })
-    },
-    isEdit: {
-      type: Boolean,
-      default: false
     }
-  });
-
-  const emit = defineEmits(['saveSuccess']);
-
-  const maxLength = 500;
-  const {getters, commit, dispatch} = useStore();
-  const userInfo = computed(() => getters['userInfo']);
-  const isLogin = computed(() => getters['isLogin']);
-  const currentLength = computed(() => richEditor.value?.totalStrLength);
-  const contentLengthExceed = computed(() => richEditor.value?.contentLengthExceed);
-  const uploadDisabled = computed(() => props.form.images.length >= 9 || !isLogin.value)
-
-  const richEditor = ref(null);
-  const active = computed(() => richEditor.value?.active);
-  const emojiVisible = ref(false);
-  const submitLoading = ref(false);
-  let position = '';
-
-  const uploadSuccess = (fileList: []) => {
-    const url = fileList[0].url + '?x-oss-process=style/smallThumb';
-    props.form.images.push(url);
+  },
+  isEdit: {
+    type: Boolean,
+    default: false
   }
+});
 
-  const toLogin = () => {
+const emit = defineEmits(['saveSuccess']);
+
+const maxLength = 500;
+const {getters, commit, dispatch} = useStore();
+const userInfo = computed(() => getters['userInfo']);
+const isLogin = computed(() => getters['isLogin']);
+const currentLength = computed(() => richEditor.value?.totalStrLength);
+const contentLengthExceed = computed(() => richEditor.value?.contentLengthExceed);
+const uploadDisabled = computed(() => props.form.images.length >= 9 || !isLogin.value)
+
+const richEditor = ref(null);
+const active = computed(() => richEditor.value?.active);
+const emojiVisible = ref(false);
+const submitLoading = ref(false);
+let position = '';
+
+const uploadSuccess = (fileList: []) => {
+  const url = fileList[0].url + '?x-oss-process=style/smallThumb';
+  props.form.images.push(url);
+}
+
+const toLogin = () => {
+  commit("changeLogin", true);
+}
+
+const onImageDelete = (index: number) => {
+  props.form.images.splice(index, 1);
+}
+
+const onSubmit = () => {
+  submitLoading.value = true;
+  const isEdit = props.isEdit;
+  const form = cloneDeep(props.form);
+  form.images = form.images.length ? form.images.join(",") : null;
+  form.content = transformHTMLToTag(form.content);
+  dispatch(isEdit ? 'updateMoment' : 'createMoment', form).then(res => {
+    message.success(isEdit ? "修改成功" : "发布成功");
+    resetEditor();
+    emit('saveSuccess', res.data);
+  }).finally(() => {
+    submitLoading.value = false;
+  })
+}
+
+const resetEditor = () => {
+  props.form.content = '';
+  props.form.images = [];
+  richEditor.value.clearContent();
+}
+
+const onImagePick = (value: HTMLElement | string) => {
+  richEditor.value.insertHtml(value)
+}
+
+const onEmojiPick = (value: string) => {
+  richEditor.value.insertText(value)
+}
+
+const onClickEmoji = () => {
+  if (!isLogin.value) {
     commit("changeLogin", true);
+    return;
   }
+  emojiVisible.value = true;
+}
 
-  const onImageDelete = (index: number) => {
-    props.form.images.splice(index, 1);
-  }
-
-  const onSubmit = () => {
-    submitLoading.value = true;
-    const isEdit = props.isEdit;
-    const form = cloneDeep(props.form);
-    form.images = form.images.length ? form.images.join(",") : null;
-    form.content = transformHTMLToTag(form.content);
-    dispatch(isEdit ? 'updateMoment' : 'createMoment', form).then(res => {
-      message.success(isEdit ? "修改成功" : "发布成功");
-      resetEditor();
-      emit('saveSuccess', res.data);
-    }).finally(() => {
-      submitLoading.value = false;
-    })
-  }
-
-  const resetEditor = () => {
-    props.form.content = '';
-    props.form.images = [];
-    richEditor.value.clearContent();
-  }
-
-  const onImagePick = (value: HTMLElement | string) => {
-    richEditor.value.insertHtml(value)
-  }
-
-  const onEmojiPick = (value: string) => {
-    richEditor.value.insertText(value)
-  }
-
-  const onClickEmoji = () => {
-    if (!isLogin.value) {
-      commit("changeLogin", true);
-      return;
-    }
-    emojiVisible.value = true;
-  }
-
-  const onEmojiClose = () => {
-    emojiVisible.value = false;
-  }
+const onEmojiClose = () => {
+  emojiVisible.value = false;
+}
 </script>
 
 <style lang="scss" scoped>
-  .moment-editor {
-    width: 750px;
-    padding: 20px 20px 0;
-    background-color: var(--youyu-background1);
-    border-radius: 4px;
+.moment-editor {
+  width: 750px;
+  padding: 20px 20px 0;
+  background-color: var(--youyu-background1);
+  border-radius: 4px;
 
-    .editor-body {
+  .editor-body {
+    position: relative;
+    background: var(--youyu-background5);
+    border-radius: 2px;
+    transition: .3s;
+
+    .login-mask {
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      z-index: 1;
+    }
+
+    .editor-content-wrapper {
       position: relative;
-      background: var(--youyu-background5);
-      border-radius: 2px;
-      transition: .3s;
+      margin-bottom: 2px;
+      max-height: 216px;
+      overflow-y: auto;
+      word-wrap: break-word;
 
-      .login-mask {
-        position: absolute;
-        left: 0;
-        right: 0;
-        top: 0;
-        bottom: 0;
-        z-index: 1;
-      }
-
-      .editor-content-wrapper {
-        position: relative;
-        margin-bottom: 2px;
-        max-height: 216px;
-        overflow-y: auto;
-        word-wrap: break-word;
-
-        ::v-deep(.editable-div) {
-          .editor-bottom {
-            padding: 4px 12px;
-          }
-        }
-      }
-
-      .topic-wrapper {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        font-size: 12px;
-
-        .now-mood {
-          padding: 0 6px;
-          line-height: 24px;
-          border-radius: 50px;
-          border-bottom-left-radius: 0;
-          background: #fff;
-          color: #1e80ff;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          cursor: pointer;
-          transition: .3s;
-
-          .i-icon {
-            position: relative;
-            top: 1px;
-          }
-
-          .mood-text {
-            padding-left: 4px;
-          }
-        }
-
-        .word-counter {
-          font-size: 13px;
-          color: var(--youyu-text1);
-          padding-right: 10px;
+      ::v-deep(.editable-div) {
+        .editor-bottom {
+          padding: 4px 12px;
         }
       }
     }
 
-    .image-wrapper {
+    .topic-wrapper {
       display: flex;
-      flex-wrap: wrap;
+      align-items: center;
+      justify-content: space-between;
+      font-size: 12px;
 
-      .image-item {
-        position: relative;
-        margin: 8px 8px 0 0;
+      .now-mood {
+        padding: 0 6px;
+        line-height: 24px;
+        border-radius: 50px;
+        border-bottom-left-radius: 0;
+        background: #fff;
+        color: #1e80ff;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+        transition: .3s;
 
-        img {
-          height: 80px;
-          width: 80px;
-          object-fit: cover;
+        .i-icon {
+          position: relative;
+          top: 1px;
         }
 
-        .image-delete {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          cursor: pointer;
-          width: 18px;
-          height: 18px;
-          position: absolute;
-          top: 4px;
-          right: 4px;
-          border-radius: 50%;
-          border: 1px solid #c5c5c5;
-          background: rgba(0, 0, 0, .4);
-
-          &:hover {
-            background: rgba(0, 0, 0, .3);
-          }
-
-          .i-icon {
-            color: white;
-          }
+        .mood-text {
+          padding-left: 4px;
         }
       }
 
-      .upload-image {
-        cursor: pointer;
+      .word-counter {
+        font-size: 13px;
+        color: var(--youyu-text1);
+        padding-right: 10px;
+      }
+    }
+  }
+
+  .image-wrapper {
+    display: flex;
+    flex-wrap: wrap;
+
+    .image-item {
+      position: relative;
+      margin: 8px 8px 0 0;
+
+      img {
+        height: 80px;
+        width: 80px;
+        object-fit: cover;
+      }
+
+      .image-delete {
         display: flex;
         justify-content: center;
         align-items: center;
-        height: 80px;
-        width: 80px;
-        border: 1px dashed #c5c5c5;
-        margin: 8px 8px 8px 0;
-        background: rgba(248, 248, 249, 0.2);
-        transition: .3s;
+        cursor: pointer;
+        width: 18px;
+        height: 18px;
+        position: absolute;
+        top: 4px;
+        right: 4px;
+        border-radius: 50%;
+        border: 1px solid #c5c5c5;
+        background: rgba(0, 0, 0, .4);
 
         &:hover {
-          background: rgba(248, 248, 249, 0.3);
+          background: rgba(0, 0, 0, .3);
+        }
+
+        .i-icon {
+          color: white;
         }
       }
     }
 
-    .editor-bottom {
-      position: relative;
+    .upload-image {
+      cursor: pointer;
       display: flex;
-      justify-content: space-between;
-      padding: 10px 0;
+      justify-content: center;
+      align-items: center;
+      height: 80px;
+      width: 80px;
+      border: 1px dashed #c5c5c5;
+      margin: 8px 8px 8px 0;
+      background: rgba(248, 248, 249, 0.2);
+      transition: .3s;
 
-      .tool-items {
+      &:hover {
+        background: rgba(248, 248, 249, 0.3);
+      }
+    }
+  }
+
+  .editor-bottom {
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 0;
+
+    .tool-items {
+      display: flex;
+      align-items: center;
+      color: var(--youyu-text2);
+
+      .tool-item {
         display: flex;
         align-items: center;
-        color: var(--youyu-text2);
+        cursor: pointer;
+        margin-right: 16px;
+        transition: .3s;
 
-        .tool-item {
-          display: flex;
-          align-items: center;
-          cursor: pointer;
-          margin-right: 16px;
-          transition: .3s;
-
-          .i-icon {
-            position: relative;
-            top: .5px;
-          }
-
-          .tool-title {
-            font-size: 13px;
-            padding-left: 4px;
-          }
-
-          &:hover {
-            color: #1890ff;
-          }
+        .i-icon {
+          position: relative;
+          top: .5px;
         }
 
-        ::v-deep(.avatar-uploader) {
-          display: flex;
+        .tool-title {
+          font-size: 13px;
+          padding-left: 4px;
         }
+
+        &:hover {
+          color: #1890ff;
+        }
+      }
+
+      ::v-deep(.avatar-uploader) {
+        display: flex;
       }
     }
   }
+}
 
-  .editor-active {
-    .editor-body {
-      background: var(--youyu-background1) !important;
+.editor-active {
+  .editor-body {
+    background: var(--youyu-background1) !important;
 
-      .now-mood {
-        background: #eaf2ff !important;
-      }
+    .now-mood {
+      background: #eaf2ff !important;
     }
   }
+}
 
-  ::v-deep(.ant-upload-disabled) {
-    cursor: not-allowed !important;
+::v-deep(.ant-upload-disabled) {
+  cursor: not-allowed !important;
 
-    .item-upload-image {
-      /*color: #8a919f !important;*/
-    }
+  .item-upload-image {
+    /*color: #8a919f !important;*/
   }
+}
 </style>
 
 <style lang="scss">
-  .emoji-picker-popover {
-    z-index: 9;
+.emoji-picker-popover {
+  z-index: 9;
 
-    .ant-popover-inner-content {
-      padding: 0;
-    }
+  .ant-popover-inner-content {
+    padding: 0;
   }
+}
 </style>
