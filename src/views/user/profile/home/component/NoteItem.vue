da15782e856dee64916bb6ec6d220edbd2c56edf
LinChaoqunRick <602314134@qq.com>
2023-08-23 22:16:10 +0800

    feat: note item

--- src/views/user/profile/home/component/NoteItem.vue
+++ src/views/user/profile/home/component/NoteItem.vue
@@ -1,5 +1,14 @@
 <template>
-{{ data}}
+  {{ data }}
+  <div class="note-item">
+    <div class="item-left">
+      <img :src="data.cover"/>
+    </div>
+    <div class="item-right">
+      <div class="note-title">{{ data.name }}</div>
+      <div class="note-introduce">{{ data.introduce }}</div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -13,5 +22,27 @@ const props = defineProps({
 </script>
 
 <style scoped lang="scss">
+.note-item {
+  display: flex;
+  padding: 12px;
+  background-color: var(--youyu-background1);
 
+  .item-left {
+    img {
+      height: 140px;
+      width: 210px;
+      object-fit: cover;
+    }
+  }
+
+  .item-right {
+    flex: 1;
+    padding-left: 12px;
+
+    .note-title {
+      font-size: 18px;
+      font-weight: bold;
+    }
+  }
+}
 </style>
