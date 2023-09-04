4d2af48b2258665f7416346296a04881367c1716
LinChaoqunRick <602314134@qq.com>
2023-09-04 21:12:52 +0800

    fix: 个人主页时刻查询错误问题

--- src/views/user/profile/moment/MomentList.vue
+++ src/views/user/profile/moment/MomentList.vue
@@ -1,25 +1,29 @@
 <template>
   <div class="moment-list">
-    <UserMomentList :userIds="user.id"/>
+    <UserMomentList :params="params"/>
   </div>
 </template>
 
 <script setup lang="ts">
-  import {computed, inject} from 'vue';
-  import {useStore} from "vuex";
-  import UserMomentList from "@/views/moment/list/UserMomentList.vue";
+import {computed, inject} from 'vue';
+import {useStore} from "vuex";
+import UserMomentList from "@/views/moment/list/UserMomentList.vue";
 
-  const {getters, commit, dispatch} = useStore();
+const {getters, commit, dispatch} = useStore();
 
-  const user = inject('user');
+const user = inject('user');
+
+const params = computed(() => ({
+  userIds: user.value.id
+}))
 </script>
 
 <style lang="scss" scoped>
-  .moment-list {
-    ::v-deep(.user-moment-list) {
-      .load-more-box {
-        margin-bottom: 0 !important;
-      }
+.moment-list {
+  ::v-deep(.user-moment-list) {
+    .load-more-box {
+      margin-bottom: 0 !important;
     }
   }
+}
 </style>
