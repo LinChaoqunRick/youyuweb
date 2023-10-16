83284afb1144020790526a980ed1ea88b39bc0a6
LinChaoqunRick <602314134@qq.com>
2023-10-16 23:05:43 +0800

    pref: 优化图片预览

--- src/components/common/utils/image/ImagePreview.vue
+++ src/components/common/utils/image/ImagePreview.vue
@@ -1,26 +1,31 @@
 <template>
   <div class="image-preview">
-    <div class="image-preview-operations">
-      <i-sort-two theme="outline" size="19" fill="currentColor" @click="handleFlip('x')"/>
-      <i-switch theme="outline" size="19" fill="currentColor" @click="handleFlip('y')"/>
-      <i-rotate theme="outline" size="20" fill="currentColor" @click="handleRotate('c')"/>
-      <i-rotate theme="outline" size="20" fill="currentColor" @click="handleRotate('ac')"
-                style="transform: scale3d(-1,1,1)"/>
-      <i-zoom-in :class="{'disabled': scale >= maxScale}" theme="outline" size="20" fill="currentColor"
-                 @click="handleScale('large')"/>
-      <i-zoom-out :class="{'disabled': scale <= minScale}" theme="outline" size="20" fill="currentColor"
-                  @click="handleScale('small')"/>
-      <i-close theme="outline" size="20" fill="currentColor" @click="onClose"/>
-      <div class="image-preview-progress" v-if="props.list.length>1">{{current+1}} / {{props.list.length}}</div>
+    <div class="image-preview-close" @click="onClose">
+      <i-close theme="outline" size="20" fill="currentColor"/>
     </div>
     <div class="image-preview-body">
       <div class="ope-icon last-icon" v-if="current!==0" @click="handleChange('last')">
         <i-left theme="outline" size="30" fill="#fff"/>
       </div>
-      <div class="ope-icon next-icon" v-if="current!==props.list.length-1" @click="handleChange('next')">
+      <div class="ope-icon next-icon" v-if="current!==props.list?.length-1" @click="handleChange('next')">
         <i-left theme="outline" size="30" fill="#fff" style="transform: scale3d(-1,1,1)"/>
       </div>
       <img :src="currentOriginUrl" @load="onLoad" id="preview-image"/>
+    </div>
+    <div class="image-preview-operations">
+      <i-left theme="outline" size="20" fill="currentColor"/>
+      <div class="image-preview-progress" v-if="props.list?.length>1">{{ current + 1 }} / {{ props.list.length }}</div>
+      <i-right class="separator" theme="outline" size="20" fill="currentColor"/>
+      <div class="divider"></div>
+      <i-zoom-in :class="{'disabled': scale >= maxScale}" theme="outline" size="19" fill="currentColor"
+                 @click="handleScale('large')"/>
+      <i-zoom-out :class="{'disabled': scale <= minScale}" theme="outline" size="19" fill="currentColor"
+                  @click="handleScale('small')"/>
+      <i-sort-two theme="outline" size="17" fill="currentColor" @click="handleFlip('x')"/>
+      <i-switch theme="outline" size="17" fill="currentColor" @click="handleFlip('y')"/>
+      <i-rotate theme="outline" size="18" fill="currentColor" @click="handleRotate('c')"/>
+      <i-rotate theme="outline" size="18" fill="currentColor" @click="handleRotate('ac')"
+                style="transform: scale3d(-1,1,1)"/>
     </div>
     <spin size="large" class="a-spin" v-show="loading"/>
   </div>
@@ -249,107 +254,138 @@
 </script>
 
 <style lang="scss" scoped>
-  .image-preview {
-    position: fixed;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    z-index: 1000;
-    background-color: rgba(0, 0, 0, .55);
+.image-preview {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 1000;
+  background-color: rgba(0, 0, 0, .8);
 
-    .image-preview-operations {
-      display: flex;
-      justify-content: flex-end;
-      align-items: center;
-      position: absolute;
-      top: 0;
-      left: 0;
-      right: 0;
-      height: 46px;
-      background: rgba(0, 0, 0, .4);
-      z-index: 1;
-      color: #fff;
+  .image-preview-close {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 48px;
+    width: 48px;
+    border-radius: 50%;
+    background: rgba(0, 0, 0, .7);
+    cursor: pointer;
+    z-index: 1;
 
-      .i-icon {
-        padding: 12px;
-        cursor: pointer;
-        transition: .3s;
-
-        &:nth-child(n+2) {
-          margin-left: 12px;
-        }
-
-        &.disabled {
-          color: #9E9E9E;
-        }
-      }
-
-      .image-preview-progress {
-        position: absolute;
-        left: 50%;
-        transform: translateX(-50%);
-      }
+    &:hover {
+      background: rgba(89, 82, 82, 0.8);
     }
 
-    .image-preview-body {
-      height: 100%;
+    .i-icon {
+      color: #fff;
+      font-weight: bold;
+    }
+  }
+
+  .image-preview-body {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    img {
+      max-width: 100%;
+      max-height: 100%;
+      vertical-align: middle;
+      transform: scale3d(1, 1, 1);
+      cursor: grab;
+      transition: transform .3s cubic-bezier(.215, .61, .355, 1) 0s;
+      user-select: none;
+      touch-action: none;
+    }
+
+    .ope-icon {
+      height: 40px;
+      width: 40px;
+      position: absolute;
+      top: 50%;
+      background-color: rgba(0, 0, 0, .1);
+      border-radius: 50%;
       display: flex;
       justify-content: center;
       align-items: center;
+      cursor: pointer;
+      z-index: 1;
 
-      img {
-        max-width: 100%;
-        max-height: 100%;
-        vertical-align: middle;
-        transform: scale3d(1, 1, 1);
-        cursor: grab;
-        transition: transform .3s cubic-bezier(.215, .61, .355, 1) 0s;
-        user-select: none;
-        touch-action: none;
-      }
-
-      .ope-icon {
-        height: 40px;
-        width: 40px;
-        position: absolute;
-        top: 50%;
-        background-color: rgba(0, 0, 0, .1);
-        border-radius: 50%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        cursor: pointer;
-        z-index: 1;
-
-        &:hover {
-          background-color: rgba(0, 0, 0, .2);
-        }
-      }
-
-      .last-icon {
-        left: 20px;
-
-        .i-icon {
-          position: relative;
-
-        }
-      }
-
-      .next-icon {
-        right: 20px;
-
-        .i-icon {
-          position: relative;
-          left: 2px;
-        }
+      &:hover {
+        background-color: rgba(0, 0, 0, .2);
       }
     }
 
-    .a-spin{
-      position: absolute;
-      left: calc(50% - 16px);
-      top: calc(50% - 16px);
+    .last-icon {
+      left: 20px;
+
+      .i-icon {
+        position: relative;
+      }
+    }
+
+    .next-icon {
+      right: 20px;
+
+      .i-icon {
+        position: relative;
+        left: 2px;
+      }
     }
   }
+
+  .image-preview-operations {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    position: absolute;
+    bottom: 40px;
+    height: 46px;
+    background: rgba(0, 0, 0, .7);
+    z-index: 1;
+    color: #fff;
+    border-radius: 6px;
+    transform: translateX(-50%);
+    left: 50%;
+    user-select: none;
+
+    > span {
+      padding: 0 8px;
+      cursor: pointer;
+      transition: .3s;
+
+      &.disabled {
+        color: #9E9E9E;
+      }
+    }
+
+    .divider {
+      width: 1px;
+      margin: 2px 10px;
+      padding: 0;
+      border-left: 1px solid hsla(0, 0%, 100%, 0.3);;
+      cursor: default;
+      height: 20px;
+      font-size: 14px;
+      line-height: 24px;
+      text-align: center;
+    }
+
+    .image-preview-progress {
+
+    }
+  }
+
+  .a-spin {
+    position: absolute;
+    left: calc(50% - 16px);
+    top: calc(50% - 16px);
+  }
+}
 </style>
