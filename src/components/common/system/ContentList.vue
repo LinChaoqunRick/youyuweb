00f5faf866f6102e0524870bccc360bbab02c108
LinChaoqunRick <602314134@qq.com>
2023-12-17 22:08:32 +0800

    fix: 底部加载提示错误

--- src/components/common/system/ContentList.vue
+++ src/components/common/system/ContentList.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="content-list" :class="{'content-list-expand': !fold}">
+  <div class="content-list" :class="{ 'content-list-expand': !fold }">
     <div
       class="data-list-wrapper"
       v-if="!!dataList?.length"
       ref="dataListWrapperRef"
     >
       <div class="data-list">
-        <slot :list="dataList"/>
+        <slot :list="dataList" />
       </div>
     </div>
     <div class="bottom-operation mt-8" ref="bottomOperation">
       <div class="failed-box" v-if="showFailed && failed" @click="onRetry">
         <slot name="failedBox">
-          <i-refresh theme="outline" size="15" fill="currentColor"/>
+          <i-refresh theme="outline" size="15" fill="currentColor" />
           <span class="ml-8">加载失败，重新加载</span>
         </slot>
       </div>
@@ -22,7 +22,8 @@
           showViewAll &&
           (total != null ? !!total : true) &&
           !fold &&
-          pageNum <= totalPageNum
+          (pageNum <= totalPageNum || totalPageNum === 0) &&
+          (!(totalPageNum === 0 && !!dataList.length))
         "
         @click="onLoadData"
       >
@@ -41,11 +42,19 @@
               size="14"
               fill="currentColor"
             />
-            <i-loading-four v-else theme="outline" size="14" fill="currentColor"/>
+            <i-loading-four
+              v-else
+              theme="outline"
+              size="14"
+              fill="currentColor"
+            />
           </div>
         </slot>
       </div>
-      <div class="no-data" v-else-if="showNoData && totalNum === 0">
+      <div
+        class="no-data"
+        v-else-if="showNoData && (totalNum === 0 || total === 0)"
+      >
         <slot name="noDataBox">暂无数据</slot>
       </div>
       <div
@@ -58,11 +67,11 @@
         <slot name="loadedAllFold">
           <div class="fold" v-show="!fold" @click="onFold(true)">
             <span>收起</span>
-            <i-up theme="outline" size="14" fill="currentColor"/>
+            <i-up theme="outline" size="14" fill="currentColor" />
           </div>
           <div class="unfold" v-show="fold" @click="onFold(false)">
             <span>展开更多{{ dataText }}</span>
-            <i-down theme="outline" size="14" fill="currentColor"/>
+            <i-down theme="outline" size="14" fill="currentColor" />
           </div>
         </slot>
       </div>
@@ -71,26 +80,28 @@
 </template>
 
 <script lang="ts" setup>
-import {ref, computed, nextTick, watch} from "vue";
-import {useStore} from "vuex";
-import {keepScrollTop} from "@/assets/utils/utils";
+import { ref, computed, nextTick, watch } from "vue";
+import { useStore } from "vuex";
+import { keepScrollTop } from "@/assets/utils/utils";
 
 const props = defineProps({
-  url: {type: String, required: true},
-  params: {type: Object},
-  showFailed: {type: Boolean, default: true},
-  showNoData: {type: Boolean, default: true},
-  showViewAll: {type: Boolean, default: true},
-  showLoadedAll: {type: Boolean, default: true},
-  foldable: {type: Boolean, default: false},
-  total: {type: Number},
-  immediate: {type: Boolean, default: true},
-  dataText: {type: String, default: "数据"},
-  autoLoad: {type: Boolean, default: false}, // 滚动到底后自动加载
-  loadTrigger: {type: Boolean, default: false}, // 配合autoLoad使用，如果是true，则需要点击后触发autoLoad
+  url: { type: String, required: true },
+  params: { type: Object },
+  showFailed: { type: Boolean, default: true },
+  showNoData: { type: Boolean, default: true },
+  showViewAll: { type: Boolean, default: true },
+  showLoadedAll: { type: Boolean, default: true },
+  foldable: { type: Boolean, default: false },
+  total: { type: Number },
+  immediate: { type: Boolean, default: true },
+  dataText: { type: String, default: "数据" },
+  autoLoad: { type: Boolean, default: false }, // 滚动到底后自动加载
+  loadTrigger: { type: Boolean, default: false }, // 配合autoLoad使用，如果是true，则需要点击后触发autoLoad
 });
 
-const {dispatch} = useStore();
+const { dispatch } = useStore();
+
+const emit = defineEmits(["update:total"]);
 
 let dataListHeight: number = 0;
 
@@ -98,8 +109,8 @@ const firstLoading = computed(
   () => !dataList.value.length && restLoading.value
 );
 const pageNum = ref<number>(1);
-const totalPageNum = ref<number>(1);
-const totalNum = ref<number>(0);
+const totalPageNum = ref<number>(0);
+const totalNum = ref<number>(props.total);
 const restLoading = ref<boolean>(false);
 const failed = ref<boolean>(false);
 const dataList = ref<Array<any>>([]);
@@ -109,7 +120,7 @@ const bottomOperation = ref<HTMLElement | null>(null);
 const dataListWrapperRef = ref<HTMLElement | null>(null);
 
 const getListData = () => {
-  if ((totalPageNum.value !== -1 && pageNum > totalPageNum) || failed.value)
+  if ((totalPageNum.value !== -1 && pageNum > totalPageNum) || failed.value || restLoading.value)
     return;
   const params = {
     pageSize: 10,
@@ -122,6 +133,7 @@ const getListData = () => {
       totalPageNum.value = res.data.pages;
       pageNum.value++;
       totalNum.value = res.data.total;
+      emit("update:total", totalNum.value);
       if (props.foldable) {
         foldAnimation();
       }
@@ -143,7 +155,7 @@ const initData = () => {
   dataList.value = [];
   totalPageNum.value = 1;
   pageNum.value = 1;
-  totalNum.value = 0;
+  totalNum.value = props.total;
   getListData();
 };
 
@@ -184,7 +196,7 @@ const onFold = (value: boolean) => {
 const foldAnimation = () => {
   nextTick(() => {
     if (dataListWrapperRef.value) {
-      ({height: dataListHeight} =
+      ({ height: dataListHeight } =
         dataListWrapperRef.value.getBoundingClientRect());
       // dataListWrapperRef.value.style.height = '0';
       // dataListWrapperRef.value.offsetHeight;
--- src/components/content/comment/CommentItem.vue
+++ src/components/content/comment/CommentItem.vue
@@ -1,27 +1,31 @@
 <template>
   <div class="comment-item">
     <div class="user-avatar">
-      <a-popover placement="top"
-                 :mouseEnterDelay="0.6"
-                 :mouseLeaveDelay="0.3"
-                 @visibleChange="onVisibleChange">
+      <a-popover
+        placement="top"
+        :mouseEnterDelay="0.6"
+        :mouseLeaveDelay="0.3"
+        @visibleChange="onVisibleChange"
+      >
         <template #content>
-          <UserCard :user="data.user"/>
+          <UserCard :user="data.user" />
         </template>
         <RouterLink :to="`/user/${data.user.id}`">
-          <img :src="data.user.avatar"/>
+          <img :src="data.user.avatar" />
         </RouterLink>
       </a-popover>
     </div>
     <div class="content-box">
       <div class="user-box">
         <div class="user-nickname">
-          <a-popover placement="top"
-                     :mouseEnterDelay="0.2"
-                     :mouseLeaveDelay="0.3"
-                     @visibleChange="onVisibleChange">
+          <a-popover
+            placement="top"
+            :mouseEnterDelay="0.2"
+            :mouseLeaveDelay="0.3"
+            @visibleChange="onVisibleChange"
+          >
             <template #content>
-              <UserCard :user="data.user"/>
+              <UserCard :user="data.user" />
             </template>
             <RouterLink :to="`/user/${data.user.id}`">
               <div class="nickname-text">{{ data.user.nickname }}</div>
@@ -29,44 +33,91 @@
           </a-popover>
           <div class="author-text" v-if="post.userId === data.userId">作者</div>
         </div>
-        <div class="create-time" :title="data.createTime">{{ $dayjs().to(data.createTime) }}</div>
+        <div class="create-time" :title="data.createTime">
+          {{ $dayjs().to(data.createTime) }}
+        </div>
       </div>
-      <div class="comment-content" :class="{'content-expand': expand}" v-row="{set: set}">
-        <MdPreview
-          :text="data.content"
-          editorId="md-editor"/>
+      <div
+        class="comment-content"
+        :class="{ 'content-expand': expand }"
+        v-row="{ set: set }"
+      >
+        <MdPreview :text="data.content" editorId="md-editor" />
       </div>
       <div class="comment-operation">
-        <div class="ope-item" :class="{'ope-active': data.commentLike}" v-login="onLike">
-          <i-good-two :theme="data.commentLike?'filled':'outline'" size="16" fill="currentColor"/>
+        <div
+          class="ope-item"
+          :class="{ 'ope-active': data.commentLike }"
+          v-login="onLike"
+        >
+          <i-good-two
+            :theme="data.commentLike ? 'filled' : 'outline'"
+            size="16"
+            fill="currentColor"
+          />
           点赞<span v-if="data.supportCount">({{ data.supportCount }})</span>
         </div>
-        <div class="ope-item" :class="{'ope-active': active}" @click="handleReply">
-          <i-comment :theme="active?'filled':'outline'" size="16" fill="currentColor"/>
-          {{ active ? '取消回复' : '回复' }}<span v-if="data.replyCount">({{ data.replyCount }})</span>
+        <div
+          class="ope-item"
+          :class="{ 'ope-active': active }"
+          @click="handleReply"
+        >
+          <i-comment
+            :theme="active ? 'filled' : 'outline'"
+            size="16"
+            fill="currentColor"
+          />
+          {{ active ? "取消回复" : "回复"
+          }}<span v-if="data.replyCount">({{ data.replyCount }})</span>
         </div>
         <div class="ope-item delete-ope" v-if="showDelete" @click="onDelete">
           删除
         </div>
       </div>
       <div class="reply-editor" v-if="active">
-        <ReplyEditor :placeholder="`回复${data.user.nickname}`" @handleSubmit="handleSubmit" ref="replyEditor"/>
+        <ReplyEditor
+          :placeholder="`回复@${data.user.nickname}`"
+          @handleSubmit="handleSubmit"
+          ref="replyEditor"
+        />
       </div>
       <div class="comment-operation">
-        <div class="limit-btn" @click="expand = true" v-show="row>7 && !expand">展开</div>
-        <div class="limit-btn" @click="expand = false" v-show="row>7 && expand">收起</div>
+        <div
+          class="limit-btn"
+          @click="expand = true"
+          v-show="row > 7 && !expand"
+        >
+          展开
+        </div>
+        <div
+          class="limit-btn"
+          @click="expand = false"
+          v-show="row > 7 && expand"
+        >
+          收起
+        </div>
       </div>
       <div class="sub-comment-wrapper" v-if="!!data.replyCount">
-        <ContentList url="getSubCommentsPage" :params="params" :immediate="false" foldable
-                     :auto-load="false" :show-loaded-all="false" :total="data.replyCount"
-                     data-text="回复" ref="ContentListRef">
-          <template v-slot="{list}">
-            <ReplyItem class="reply-item"
-                       :activeId="activeId"
-                       v-for="item in list"
-                       :data="item"
-                       @saveSuccess="saveSuccess"
-                       @deleteSuccess="deleteSuccess"/>
+        <ContentList
+          url="getSubCommentsPage"
+          :params="params"
+          :immediate="false"
+          foldable
+          :auto-load="false"
+          :show-loaded-all="false"
+          :total="data.replyCount"
+          data-text="回复"
+          ref="ContentListRef"
+        >
+          <template v-slot="{ list }">
+            <ReplyItem
+              class="reply-item"
+              :activeId="activeId"
+              v-for="item in list"
+              :data="item"
+              @saveSuccess="saveSuccess"
+              @deleteSuccess="deleteSuccess"
+            />
           </template>
         </ContentList>
       </div>
@@ -75,46 +126,50 @@
 </template>
 
 <script setup lang="ts">
-import {ref, computed, inject} from 'vue';
-import type {PropType} from "vue";
-import {useStore} from "vuex";
-import {useRouter, RouterLink} from "vue-router";
-import {message, Modal} from "ant-design-vue";
-import type {userType} from "@/types/user";
+import { ref, computed, inject, nextTick } from "vue";
+import type { PropType } from "vue";
+import { useStore } from "vuex";
+import { useRouter, RouterLink } from "vue-router";
+import { message, Modal } from "ant-design-vue";
+import type { userType } from "@/types/user";
 import ReplyItem from "@/components/content/comment/ReplyItem.vue";
 import UserCard from "@/components/content/comment/UserCard.vue";
 import ReplyEditor from "@/components/content/comment/ReplyEditor.vue";
 import MdPreview from "@/components/content/mdEditor/MdPreview.vue";
 import ContentList from "@/components/common/system/ContentList.vue";
 
-const {getters, commit, dispatch} = useStore();
+const { getters, commit, dispatch } = useStore();
 const router = useRouter();
-const isLogin = computed(() => getters['isLogin']);
-const userInfo = computed(() => getters['userInfo']);
+const isLogin = computed(() => getters["isLogin"]);
+const userInfo = computed(() => getters["userInfo"]);
 
 const props = defineProps({
   data: {
     type: Object,
-    required: true
+    required: true,
   },
 });
 
-const showDelete = computed(() => userInfo.value.id === props.data.userId || post.value.userId === userInfo.value.id)
-const post = inject('post');
-const setPostAttribute = inject('setPostAttribute');
+const showDelete = computed(
+  () =>
+    userInfo.value.id === props.data.userId ||
+    post.value.userId === userInfo.value.id
+);
+const post = inject("post");
+const setPostAttribute = inject("setPostAttribute");
 const expand = ref<boolean>(false);
 const row = ref<number>(0);
 const replyEditor = ref(null);
-const {activeId, updateActiveId} = inject('active');
+const { activeId, updateActiveId } = inject("active");
 const active = computed(() => activeId.value === props.data.id);
 const likeLoading = ref<boolean>(false);
 const ContentListRef = ref(null);
 
 const params = computed(() => ({
-  commentId: props.data.id
-}))
+  commentId: props.data.id,
+}));
 
-const emit = defineEmits(['deleteSuccess'])
+const emit = defineEmits(["deleteSuccess"]);
 
 function set(value: number) {
   row.value = value;
@@ -136,21 +191,23 @@ function onLike() {
   if (likeLoading.value) return;
   likeLoading.value = true;
   const isLike = props.data.commentLike;
-  dispatch(isLike ? 'cancelCommentLike' : 'setCommentLike', {
+  dispatch(isLike ? "cancelCommentLike" : "setCommentLike", {
     commentId: props.data.id,
     userId: userInfo.value.id,
-    userIdTo: props.data.userId
-  }).then(res => {
-    if (isLike) {
-      props.data.commentLike = null;
-      props.data.supportCount -= 1;
-    } else {
-      props.data.commentLike = res.data;
-      props.data.supportCount += 1;
-    }
-  }).finally(() => {
-    likeLoading.value = false;
+    userIdTo: props.data.userId,
   })
+    .then((res) => {
+      if (isLike) {
+        props.data.commentLike = null;
+        props.data.supportCount -= 1;
+      } else {
+        props.data.commentLike = res.data;
+        props.data.supportCount += 1;
+      }
+    })
+    .finally(() => {
+      likeLoading.value = false;
+    });
 }
 
 const handleSubmit = (content: string, submitCallback: Function) => {
@@ -159,55 +216,64 @@ const handleSubmit = (content: string, submitCallback: Function) => {
     userId: userInfo.value.id,
     userIdTo: props.data.userId,
     rootId: props.data.id,
-    content: content
-  }).then(res => {
-    ContentListRef.value.list.unshift(res.data);
-    message.success('评论成功');
-    updateActiveId(-1);
-    setPostAttribute('commentCount', post.value.commentCount + 1);
-    props.data.replyCount = props.data.replyCount + 1;
-  }).catch(e => {
-    message.error("评论失败")
-  }).finally(() => submitCallback())
-}
+    content: content,
+  })
+    .then((res) => {
+      props.data.replyCount = props.data.replyCount + 1;
+      nextTick(() => {
+        ContentListRef.value?.list.unshift(res.data);
+        message.success("评论成功");
+        updateActiveId(-1);
+        setPostAttribute("commentCount", post.value.commentCount + 1);
+      });
+    })
+    .catch((e) => {
+      message.error("评论失败");
+    })
+    .finally(() => submitCallback());
+};
 
 function onDelete() {
   Modal.confirm({
-    title: '删除评论',
-    icon: '', // <help theme="outline" size="24" fill="#1890ff"/>
-    content: '确定删除这条评论吗？',
+    title: "删除评论",
+    icon: "", // <help theme="outline" size="24" fill="#1890ff"/>
+    content: "确定删除这条评论吗？",
     onOk() {
-      return dispatch("deleteComment", {commentId: props.data.id}).then(res => {
-        if (res.data) {
-          message.success('删除成功');
-          emit("deleteSuccess", props.data);
-        } else {
-          message.error('删除失败');
-        }
-      }).catch(console.log)
+      return dispatch("deleteComment", { commentId: props.data.id })
+        .then((res) => {
+          if (res.data) {
+            message.success("删除成功");
+            emit("deleteSuccess", props.data);
+          } else {
+            message.error("删除失败");
+          }
+        })
+        .catch(console.log);
     },
   });
 }
 
 const onVisibleChange = (visible: boolean) => {
   if (visible) {
-    dispatch('getPostUserById', {userId: props.data.user.id}).then(res => {
+    dispatch("getPostUserById", { userId: props.data.user.id }).then((res) => {
       props.data.user = res.data;
-    })
+    });
   }
-}
+};
 
 const saveSuccess = (data) => {
   ContentListRef.value.list.unshift(data);
-  setPostAttribute('commentCount', post.value.commentCount + 1);
+  setPostAttribute("commentCount", post.value.commentCount + 1);
   props.data.replyCount = props.data.replyCount + 1;
-}
+};
 
 const deleteSuccess = (data) => {
-  ContentListRef.value.list = ContentListRef.value.list.filter(item => item.id !== data.id);
-  setPostAttribute('commentCount', post.value.commentCount - 1);
+  ContentListRef.value.list = ContentListRef.value.list.filter(
+    (item) => item.id !== data.id
+  );
+  setPostAttribute("commentCount", post.value.commentCount - 1);
   props.data.replyCount = props.data.replyCount - 1;
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -334,7 +400,6 @@ const deleteSuccess = (data) => {
       }
 
       &:hover {
-
       }
     }
 
@@ -343,11 +408,8 @@ const deleteSuccess = (data) => {
     }
 
     .sub-comment-wrapper {
-
       ::v-deep(.content-list) {
-
         .data-list-wrapper {
-
         }
 
         .data-list {
@@ -385,7 +447,6 @@ const deleteSuccess = (data) => {
   &:hover {
     .comment-operation {
       .ope-item {
-
         &.delete-ope {
           display: inherit;
         }
