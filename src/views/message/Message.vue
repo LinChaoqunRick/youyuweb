91c81b09ca33b6c6f3858d54e7839d8899c7be39
LinChaoqunRick <602314134@qq.com>
2024-02-23 23:13:40 +0800

    feat: 留言

--- src/views/message/Message.vue
+++ src/views/message/Message.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="message">
-    留言
+    <a-form :model="formState" name="basic" :label-col="{ span: 8 }" :wrapper-col="{ span: 16 }" autocomplete="off">
+      <a-form-item label="Username" name="username" :rules="[{ required: true, message: 'Please input your username!' }]">
+        <a-input v-model:value="formState.username" />
+      </a-form-item>
+
+      <a-form-item label="Password" name="password" :rules="[{ required: true, message: 'Please input your password!' }]">
+        <a-input-password v-model:value="formState.password" />
+      </a-form-item>
+
+      <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
+        <a-button type="primary" html-type="submit">Submit</a-button>
+      </a-form-item>
+    </a-form>
   </div>
 </template>
 
 <script setup lang="ts">
+import { reactive } from 'vue';
 
+const formState = reactive({
+  username: '',
+  password: '',
+});
 </script>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
