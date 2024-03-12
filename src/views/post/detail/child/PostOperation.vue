870df2380c88b437158d7c520c7dc39e4ae85a47
linchaoqun <602314134@qq.com>
2024-03-12 20:17:51 +0800

    style: 优化文章操作栏样式

--- src/views/post/detail/child/PostOperation.vue
+++ src/views/post/detail/child/PostOperation.vue
@@ -23,7 +23,7 @@
     <div class="ope-item" title="举报">
       <i-caution theme="filled" size="19" fill="currentColor"/>
     </div>
-    <a-back-top>
+    <a-back-top class="ant-back-top">
       <div class="ope-item" ref="item" title="返回顶部">
         <i-to-top-one theme="filled" size="21" fill="currentColor"/>
       </div>
@@ -32,116 +32,124 @@
 </template>
 
 <script lang="ts" setup>
-  import {computed, inject, reactive, ref} from "vue";
-  import {useStore} from "vuex";
-  import {message} from 'ant-design-vue';
+import {computed, inject, reactive, ref} from "vue";
+import {useStore} from "vuex";
+import {message} from 'ant-design-vue';
 
-  const {getters, commit, dispatch} = useStore();
+const {getters, commit, dispatch} = useStore();
 
-  const emit = defineEmits(['scrollToComment']);
-  const post = inject('post');
-  const setPostAttribute = inject('setPostAttribute');
-  const item = ref(null);
-  const likeLoading = ref<boolean>(false);
-  const collectLoading = ref<boolean>(false);
+const emit = defineEmits(['scrollToComment']);
+const post = inject('post');
+const setPostAttribute = inject('setPostAttribute');
+const item = ref(null);
+const likeLoading = ref<boolean>(false);
+const collectLoading = ref<boolean>(false);
 
-  const userInfo = computed(() => getters['userInfo']);
-  const isLogin = computed(() => getters['isLogin']);
+const userInfo = computed(() => getters['userInfo']);
+const isLogin = computed(() => getters['isLogin']);
 
-  function scrollToComment() {
-    emit('scrollToComment');
-  }
+function scrollToComment() {
+  emit('scrollToComment');
+}
 
-  function onLike() {
-    if (likeLoading.value) return;
-    likeLoading.value = true;
-    const isLike = post.value.postLike;
-    dispatch(isLike ? "cancelPostLike" : "setPostLike", {
-      postId: post.value.id,
-      userId: userInfo.value.id,
-      userIdTo: post.value.userId
-    }).then(res => {
-      if (isLike) {
-        setPostAttribute('postLike', false);
-        setPostAttribute('likeCount', post.value.likeCount - 1);
-      } else {
-        setPostAttribute('postLike', true);
-        setPostAttribute('likeCount', post.value.likeCount + 1);
-      }
-    }).finally(() => {
-      likeLoading.value = false;
-    })
-  }
+function onLike() {
+  if (likeLoading.value) return;
+  likeLoading.value = true;
+  const isLike = post.value.postLike;
+  dispatch(isLike ? "cancelPostLike" : "setPostLike", {
+    postId: post.value.id,
+    userId: userInfo.value.id,
+    userIdTo: post.value.userId
+  }).then(res => {
+    if (isLike) {
+      setPostAttribute('postLike', false);
+      setPostAttribute('likeCount', post.value.likeCount - 1);
+    } else {
+      setPostAttribute('postLike', true);
+      setPostAttribute('likeCount', post.value.likeCount + 1);
+    }
+  }).finally(() => {
+    likeLoading.value = false;
+  })
+}
 
-  function onCollect() {
-    if (collectLoading.value) return;
-    collectLoading.value = true;
-    const isCollect = post.value.postCollect;
-    dispatch(isCollect ? "cancelPostCollect" : "setPostCollect", {
-      postId: post.value.id,
-      userId: userInfo.value.id,
-      userIdTo: post.value.userId
-    }).then(res => {
-      if (isCollect) {
-        setPostAttribute('postCollect', false);
-        setPostAttribute('collectCount', post.value.collectCount - 1);
-        message.success('已从您的收藏夹中移除');
-      } else {
-        setPostAttribute('postCollect', true);
-        setPostAttribute('collectCount', post.value.collectCount + 1);
-        message.success('已添加至您的收藏夹');
-      }
-    }).finally(() => {
-      collectLoading.value = false;
-    })
-  }
+function onCollect() {
+  if (collectLoading.value) return;
+  collectLoading.value = true;
+  const isCollect = post.value.postCollect;
+  dispatch(isCollect ? "cancelPostCollect" : "setPostCollect", {
+    postId: post.value.id,
+    userId: userInfo.value.id,
+    userIdTo: post.value.userId
+  }).then(res => {
+    if (isCollect) {
+      setPostAttribute('postCollect', false);
+      setPostAttribute('collectCount', post.value.collectCount - 1);
+      message.success('已从您的收藏夹中移除');
+    } else {
+      setPostAttribute('postCollect', true);
+      setPostAttribute('collectCount', post.value.collectCount + 1);
+      message.success('已添加至您的收藏夹');
+    }
+  }).finally(() => {
+    collectLoading.value = false;
+  })
+}
 </script>
 
 <style lang="scss" scoped>
-  .post-operation {
-    .ope-item {
-      height: 3.8rem;
-      width: 3.8rem;
-      background-color: var(--youyu-body-background3);
-      border-radius: 50%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      margin-bottom: 7px;
-      cursor: pointer;
-      color: #8a919f;
+.post-operation {
+  .ope-item {
+    height: 3.8rem;
+    width: 3.8rem;
+    background-color: var(--youyu-body-background3);
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 7px;
+    cursor: pointer;
+    color: #8a919f;
 
+    .i-icon {
+      transition: .3s;
+    }
+
+    &.active {
       .i-icon {
-        transition: .3s;
-      }
-
-      &.active {
-        .i-icon {
-          color: #1890ff !important;
-        }
-      }
-
-      &:hover {
-        .i-icon {
-          color: #545457;
-        }
+        color: #1890ff !important;
       }
     }
 
-    ::v-deep(.ant-back-top) {
-      position: relative;
-      top: 0;
-      left: 0;
-    }
-
-    ::v-deep(.ant-badge) {
-      display: block;
-
-      .ant-badge-count {
-        top: 5px;
-        right: 2px;
-        color: #ffffff !important;
+    &:hover {
+      .i-icon {
+        color: #545457;
       }
     }
   }
+
+  .ant-back-top {
+    position: relative;
+    top: 0;
+    left: 0;
+    height: 3.8rem;
+    width: 3.8rem;
+    background-color: var(--youyu-body-background3) !important;
+    border-radius: 50%;
+
+    ::v-deep(.ant-float-btn-body) {
+      background: transparent !important;
+    }
+  }
+
+  ::v-deep(.ant-badge) {
+    display: block;
+
+    .ant-badge-count {
+      top: 5px;
+      right: 2px;
+      color: #ffffff !important;
+    }
+  }
+}
 </style>
