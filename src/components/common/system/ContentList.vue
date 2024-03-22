01416eec29ad6f5e869bc29cc8f6b37c30d02d33
linchaoqun <602314134@qq.com>
2024-03-22 17:02:51 +0800

    fix: 修复相关显示、加载逻辑问题

--- src/components/common/system/ContentList.vue
+++ src/components/common/system/ContentList.vue
@@ -3,124 +3,103 @@
     <div
       class="data-list-wrapper"
       v-if="!!dataList?.length"
-      ref="dataListWrapperRef"
-    >
+      ref="dataListWrapperRef">
       <div class="data-list">
-        <slot :list="dataList" />
+        <slot :list="dataList"/>
       </div>
     </div>
     <div class="bottom-operation mt-8" ref="bottomOperation">
       <div class="failed-box" v-if="showFailed && failed" @click="onRetry">
         <slot name="failedBox">
-          <i-refresh theme="outline" size="15" fill="currentColor" />
+          <i-refresh theme="outline" size="15" fill="currentColor"/>
           <span class="ml-8">加载失败，重新加载</span>
         </slot>
       </div>
-      <div
-        class="no-data"
-        v-else-if="showNoData && (totalNum === 0 || total === 0)"
-      >
-        <slot name="noDataBox">暂无数据</slot>
-      </div>
-      <div
-        class="view-all-data"
-        v-else-if="
+      <div class="success-box" v-else>
+        <div
+          class="view-all-data"
+          v-if="
           showViewAll &&
           (total != null ? !!total : true) &&
           !fold &&
           (pageNum <= totalPageNum || totalPageNum === 0) &&
-          (!(totalPageNum === 0 && !!dataList.length))
-        "
-        @click="onLoadData"
-      >
-        <slot name="loadMoreBox" :loading="restLoading" :total="totalNum">
-          <div class="load-more-item">
-            <div>
-              {{
-                pageNum === 1
-                  ? `${total}&nbsp;条${dataText}`
-                  : `查看更多${dataText}`
-              }}
+          (firstLoading ? true : restNum > 0)"
+          @click="onLoadData">
+          <slot name="loadMoreBox" :loading="restLoading" :total="totalNum">
+            <div class="load-more-item mr-10">
+              <div>{{ pageNum === 1 ? `${total}&nbsp;条${dataText}` : `查看更多${dataText}` }}
+              </div>
+              <i-down v-if="!restLoading" theme="outline" size="14" fill="currentColor"/>
+              <i-loading-four v-else theme="outline" size="14" fill="currentColor"/>
             </div>
-            <i-down
-              v-if="!restLoading"
-              theme="outline"
-              size="14"
-              fill="currentColor"
-            />
-            <i-loading-four
-              v-else
-              theme="outline"
-              size="14"
-              fill="currentColor"
-            />
-          </div>
-        </slot>
-      </div>
-      <div
-        class="loaded-all-data"
-        v-else-if="showLoadedAll && pageNum > totalPageNum"
-      >
-        <slot name="loadedAllBox">已加载全部{{ dataText }} ~</slot>
-      </div>
-      <div class="loaded-fold" v-if="foldable && !!dataList?.length">
-        <slot name="loadedAllFold">
-          <div class="fold" v-show="!fold" @click="onFold(true)">
-            <span>收起</span>
-            <i-up theme="outline" size="14" fill="currentColor" />
-          </div>
-          <div class="unfold" v-show="fold" @click="onFold(false)">
-            <span>展开更多{{ dataText }}</span>
-            <i-down theme="outline" size="14" fill="currentColor" />
-          </div>
-        </slot>
+          </slot>
+        </div>
+        <div
+          class="loaded-all-data"
+          v-else-if="showLoadedAll && pageNum > totalPageNum">
+          <slot name="loadedAllBox">已加载全部{{ dataText }} ~</slot>
+        </div>
+        <div class="no-data" v-else-if="showNoData && (totalNum === 0 || total === 0)">
+          <slot name="noDataBox">暂无数据</slot>
+        </div>
+        <div class="loaded-fold" v-if="foldable && !!dataList?.length">
+          <slot name="loadedAllFold">
+            <div class="fold" v-show="!fold" @click="onFold(true)">
+              <span>收起</span>
+              <i-up theme="outline" size="14" fill="currentColor"/>
+            </div>
+            <div class="unfold" v-show="fold" @click="onFold(false)">
+              <span>展开更多{{ dataText }}</span>
+              <i-down theme="outline" size="14" fill="currentColor"/>
+            </div>
+          </slot>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, computed, nextTick, watch } from "vue";
-import { useStore } from "vuex";
-import { keepScrollTop } from "@/assets/utils/utils";
+import {ref, computed, nextTick, watch} from "vue";
+import {useStore} from "vuex";
+import {keepScrollTop} from "@/assets/utils/utils";
 
 const props = defineProps({
-  url: { type: String, required: true },
-  params: { type: Object },
-  showFailed: { type: Boolean, default: true },
-  showNoData: { type: Boolean, default: true },
-  showViewAll: { type: Boolean, default: true },
-  showLoadedAll: { type: Boolean, default: true },
-  foldable: { type: Boolean, default: false },
-  total: { type: Number },
-  immediate: { type: Boolean, default: true },
-  dataText: { type: String, default: "数据" },
-  autoLoad: { type: Boolean, default: false }, // 滚动到底后自动加载
-  loadTrigger: { type: Boolean, default: false }, // 配合autoLoad使用，如果是true，则需要点击后触发autoLoad
+  url: {type: String, required: true},
+  params: {type: Object},
+  showFailed: {type: Boolean, default: true},
+  showNoData: {type: Boolean, default: true},
+  showViewAll: {type: Boolean, default: true},
+  showLoadedAll: {type: Boolean, default: true},
+  foldable: {type: Boolean, default: false},
+  total: {type: Number},
+  immediate: {type: Boolean, default: true},
+  dataText: {type: String, default: "数据"},
+  autoLoad: {type: Boolean, default: false}, // 滚动到底后自动加载
+  loadTrigger: {type: Boolean, default: false}, // 配合autoLoad使用，如果是true，则需要点击后触发autoLoad
 });
 
-const { dispatch } = useStore();
+const {dispatch} = useStore();
 
 const emit = defineEmits(["update:total"]);
 
 let dataListHeight: number = 0;
 
-const firstLoading = computed(
-  () => !dataList.value.length && restLoading.value
-);
+const firstLoading = computed(() => !dataList.value.length && restLoading.value);
 const pageNum = ref<number>(1);
 const totalPageNum = ref<number>(0);
-const totalNum = ref<number>(props.total);
+const totalNum = ref<number>(props.total ?? 0);
 const restLoading = ref<boolean>(false);
 const failed = ref<boolean>(false);
 const dataList = ref<Array<any>>([]);
 const fold = ref<boolean>(false);
+const restNum = computed(() => (props.total ?? totalNum.value) - dataList.value.length)
 
 const bottomOperation = ref<HTMLElement | null>(null);
 const dataListWrapperRef = ref<HTMLElement | null>(null);
 
 const getListData = () => {
-  if ((totalPageNum.value !== -1 && pageNum > totalPageNum) || failed.value || restLoading.value || totalNum.value === 0)
+  if ((totalPageNum.value !== -1 && pageNum > totalPageNum) || failed.value || restLoading.value)
     return;
   const params = {
     pageSize: 10,
@@ -129,7 +108,11 @@ const getListData = () => {
   restLoading.value = true;
   dispatch(props.url, Object.assign({}, params, props.params))
     .then((res) => {
-      dataList.value.push(...res.data.list);
+      if (pageNum.value === 1) {
+        dataList.value = res.data.list;
+      } else {
+        dataList.value.push(...res.data.list);
+      }
       totalPageNum.value = res.data.pages;
       pageNum.value++;
       totalNum.value = res.data.total;
@@ -170,8 +153,9 @@ const onLoadData = () => {
 const onUnlock = () => {
   if (!bottomOperation.value) return;
   const ob = new IntersectionObserver((entries) => {
-    if (!entries[0].isIntersecting || (pageNum.value >= totalPageNum.value)) return;
-    getListData();
+    if (entries[0].isIntersecting && pageNum.value <= totalPageNum.value) {
+      getListData();
+    }
   });
   ob.observe(bottomOperation.value);
 };
@@ -196,7 +180,7 @@ const onFold = (value: boolean) => {
 const foldAnimation = () => {
   nextTick(() => {
     if (dataListWrapperRef.value) {
-      ({ height: dataListHeight } =
+      ({height: dataListHeight} =
         dataListWrapperRef.value.getBoundingClientRect());
       // dataListWrapperRef.value.style.height = '0';
       // dataListWrapperRef.value.offsetHeight;
