7feb1fba36d414ea5a161cde42f55c38202ae0af
LinChaoqunRick <602314134@qq.com>
2024-04-01 23:14:34 +0800

    feat: threejs loaders

--- src/libs/three/loaders.ts
+++ src/libs/three/loaders.ts
@@ -1,10 +1,13 @@
-import { TextureLoader } from 'three';
+import { Loader, TextureLoader } from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 
 interface loaderConfig {
   onProcess?: Function;
 }
 
-const TextureLoaderAsync = (path: string, config: loaderConfig = {}) => {
+const CreateLoader = (loader: Loader, path: string, config: loaderConfig = {}) => {
   return new Promise((resolve, reject) => {
     const onLoad = texture => {
       return resolve(texture);
@@ -24,4 +27,9 @@ const TextureLoaderAsync = (path: string, config: loaderConfig = {}) => {
   });
 };
 
-export { TextureLoaderAsync };
+const TextureLoaderAsync = (path: string, config: loaderConfig) => CreateLoader(new TextureLoader(), path, config);
+const GLTFLoaderAsync = (path: string, config: loaderConfig) => CreateLoader(new GLTFLoader(), path, config);
+const MTLLoaderAsync = (path: string, config: loaderConfig) => CreateLoader(new MTLLoader(), path, config);
+const ObjLoaderAsync = (path: string, config: loaderConfig) => CreateLoader(new OBJLoader(), path, config);
+
+export { TextureLoaderAsync, GLTFLoaderAsync, MTLLoaderAsync, ObjLoaderAsync };
