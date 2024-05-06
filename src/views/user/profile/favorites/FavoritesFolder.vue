856fb4704477f584ef61921b975ef405e4127d95
linchaoqun <602314134@qq.com>
2024-05-06 16:56:58 +0800

    feat: 新增收藏夹相关页面

--- src/views/user/profile/favorites/FavoritesFolder.vue
+++ src/views/user/profile/favorites/FavoritesFolder.vue
@@ -1,49 +1,24 @@
 <template>
   <div class="collect-list">
     <div class="list-body">
-      <YTable listUrl="getPostCollectList" :params="listParams" ref="yTable">
-        <template #default="{dataList}">
-          <div v-for="(item, index) in dataList" class="article-item" :key="item.postId">
-            <PostItem :data="item" :index="index">
-              <template #footer>
-                <a-button v-if="isOwn" size="small" type="primary" danger @click="onRemove(item)">移除</a-button>
-              </template>
-            </PostItem>
-          </div>
-        </template>
-      </YTable>
+      <ContentData url="listFavorites" :params="listParams" v-slot="{data}" ref="ContentDataRef">
+        <div v-for="(item, index) in data" class="article-item" :key="item.id">
+          {{ item }}
+        </div>
+      </ContentData>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import {useStore} from "vuex";
-import {computed, inject, ref} from "vue";
-import {message} from "ant-design-vue";
-import YTable from "@/components/common/table/YTable.vue";
-import PostItem from "../post/PostItem.vue";
+import {computed, inject} from "vue";
+import ContentData from "@/components/common/system/ContentData.vue";
 
-const {getters, dispatch} = useStore();
 const user = inject('user');
-const yTable = ref(null);
-const userInfo = computed(() => getters['userInfo']);
 const listParams = computed(() => ({
   userId: user.value.id,
-  pageSize: 20
 }))
-const isOwn = computed(() => userInfo.value.id === user.value.id);
 
-const onRemove = (item) => {
-  dispatch("cancelPostCollect", {
-    postId: item.id,
-    userId: userInfo.value.id,
-  }).then(res => {
-    if (res.data) {
-      message.success("已从您的收藏夹中移除");
-      yTable.value.initData();
-    }
-  })
-}
 </script>
 
 <style lang="scss" scoped>
---
+++ src/views/user/profile/favorites/components/FavoritesItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="favorites-item">
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import type {PropType} from "vue";
+
+interface FavoritesType {
+  id?: string,
+  userId?: number,
+  name?: string,
+  postIds?: string,
+  cover?: string,
+  open?: number,
+  isTop?: number,
+  likeCount?: number,
+  createTime?: string,
+  updateTime?: string,
+}
+
+const props = defineProps({
+  data: {
+    type: Object as PropType<FavoritesType>,
+    require: true
+  }
+})
+</script>
+
+<style scoped lang="scss">
+
+</style>
