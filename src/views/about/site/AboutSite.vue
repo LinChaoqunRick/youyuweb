c0803f98fa1070d14b7b6808450c08bdb24ed2b4
LinChaoqunRick <602314134@qq.com>
2024-07-30 07:56:16 +0800

    feat: 实验性组件封装

--- src/views/about/site/AboutSite.vue
+++ src/views/about/site/AboutSite.vue
@@ -1,20 +1,23 @@
 <template>
-  <div>
-
-  </div>
+  <MyInput a="1" b="2" c="3" v-model="val" ref="myInputRef">
+    <template #prefix>
+      prefix
+    </template>
+    <template #suffix>
+      suffix
+    </template>
+  </MyInput>
 </template>
 
-<script>
-  export default {
-    name: "AboutSite",
-    data() {
-      return {
-        siteValue: ''
-      }
-    }
-  }
+<script setup lang="ts">
+import MyInput from '@/views/about/site/MyInput.vue';
+import { onMounted, ref } from 'vue';
+
+const myInputRef = ref(null);
+
+onMounted(() => {
+  console.log(myInputRef.value?.focus);
+  myInputRef.value?.focus();
+});
 </script>
 
-<style scoped>
-
-</style>
---
+++ src/views/about/site/MyInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <a-input ref="aInputRef">
+    <template v-for="(_, name) in $slots" #[name]="slotData">
+      <slot :name="name" v-bind="slotData || {}"></slot>
+    </template>
+  </a-input>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+const aInputRef = ref(null);
+
+const inputProps = ref({})
+
+onMounted(() => {
+  for (const key in aInputRef.value) {
+    inputProps[key] = aInputRef.value[key];
+  }
+});
+
+defineExpose(aInputRef.value);
+</script>
+
